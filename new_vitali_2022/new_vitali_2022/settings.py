5f71dff7445f054426e4c7eb0357cfe7d951a25b
drakonha <drakohha@tut.by>
2022-07-20 17:50:27 +0300

    не отображет все

--- new_vitali_2022/new_vitali_2022/settings.py
+++ new_vitali_2022/new_vitali_2022/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'new_vitali_2022.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['new_vitali_2022/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- new_vitali_2022/new_vitali_2022/views.py
+++ new_vitali_2022/new_vitali_2022/views.py
@@ -1,10 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def about(request):
-    a=5+5
-    return  render(request, 'about.html', {'gretting':a}) #для отправки переменных
 
 
-def home(request):
-    return HttpResponse('This is my home')
+
+
